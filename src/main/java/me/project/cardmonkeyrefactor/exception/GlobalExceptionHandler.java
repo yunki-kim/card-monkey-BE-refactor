c07915e38aab8db4a55af7f27759d3f01b2e7487
Yuki <skool109@gmail.com>
2023-04-05 14:09:46 +0900

    refactor: Create valid Exception
    Valid Exception 처리 핸들러 추가
    + 공통적인 에러코드를 담기위한 enum 클래스 생성
    + @Valid에 의해 발생된 예외를 처리 및 응답값을 생성하도록 구현
    
    Resolves: #5

--- src/main/java/me/project/cardmonkeyrefactor/exception/GlobalExceptionHandler.java
+++ src/main/java/me/project/cardmonkeyrefactor/exception/GlobalExceptionHandler.java
@@ -1,9 +1,17 @@
 package me.project.cardmonkeyrefactor.exception;
 
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
@@ -13,6 +21,14 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
         ErrorCode errorCode = e.getErrorCode();
 
         return handleExceptionInternal(errorCode);
+    }
+
+    @Override
+    public ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException e, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        ErrorCode errorCode = CommonErrorCode.INVALID_INPUT;
+
+        return handleExceptionInternal(e, errorCode);
     }
 
     private ResponseEntity<Object> handleExceptionInternal(ErrorCode errorCode) {
@@ -25,6 +41,25 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
         return ErrorResponse.builder()
                 .code(errorCode.name())
                 .message(errorCode.getMessage())
+                .build();
+    }
+
+    private ResponseEntity<Object> handleExceptionInternal(BindException e, ErrorCode errorCode) {
+        return ResponseEntity.status(errorCode.getHttpStatus())
+                .body(makeErrorResponse(e, errorCode));
+    }
+
+    private ErrorResponse makeErrorResponse(BindException e, ErrorCode errorCode) {
+        List<ErrorResponse.ValidationError> validationErrorList = e.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(ErrorResponse.ValidationError::of)
+                .collect(Collectors.toList());
+
+        return ErrorResponse.builder()
+                .code(errorCode.name())
+                .message(errorCode.getMessage())
+                .errors(validationErrorList)
                 .build();
     }
 }
