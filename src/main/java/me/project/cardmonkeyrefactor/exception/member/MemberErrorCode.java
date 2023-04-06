a8843324837c68954a71d69fadf0ff92fd7d567f
Yuki <skool109@gmail.com>
2023-04-06 15:06:07 +0900

    refactor: Create already in use id exception
    이미 사용중인 아이디에 대한 예외처리 추가
    
    Fixes: #7

--- src/main/java/me/project/cardmonkeyrefactor/exception/member/MemberErrorCode.java
+++ src/main/java/me/project/cardmonkeyrefactor/exception/member/MemberErrorCode.java
@@ -10,7 +10,8 @@ import org.springframework.http.HttpStatus;
 public enum MemberErrorCode implements ErrorCode {
 
     MEMBER_NOT_FOUND(HttpStatus.BAD_REQUEST, "해당 회원 정보를 찾을 수 없습니다."),
-    DUPLICATE_INPUT_PASSWORD(HttpStatus.BAD_REQUEST, "현재 비밀번호와 새 비밀번호는 동일할 수 없습니다.");
+    DUPLICATE_INPUT_PASSWORD(HttpStatus.BAD_REQUEST, "현재 비밀번호와 새 비밀번호는 동일할 수 없습니다."),
+    ALREADY_USE_ID(HttpStatus.CONFLICT, "이미 사용중인 아이디입니다. 다른 아이디를 입력해주세요.");
 
     private final HttpStatus httpStatus;
     private final String message;
